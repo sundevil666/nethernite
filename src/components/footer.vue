a488ad29403f7cc56c857000e171c2f273e895e6
sundevil <sundevildi@gmail.com>
2021-02-23 19:51:42 +0200

    add foto

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <p><img src="../assets/foto.png" alt=""></p>
     <p>author: Сергей</p>
     <p>url: <a href="https://github.com/sundevil666/nethernite.git" target="_blank">Github</a></p>
   </div>
